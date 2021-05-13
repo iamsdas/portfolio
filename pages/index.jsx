f573d133477224f1538ce9f75569024ed8ea6868
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-05-13 14:05:16 +0530

    add basic meta tags for seo

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,25 @@
 import About from '../components/About';
 import Banner from '../components/Banner';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta charset='UTF-8' />
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta
+          name='description'
+          content="Suryashankar Das's (iamsdas) personal portfolio webiste"
+        />
+        <meta
+          name='google-site-verification'
+          content='nhvnz7qvhSlwPw0IQj0cV7JXdsmpbiJ_nvbFz-N-a90'
+        />
+        <title>Portfolio | Suryashankar Das (iamsdas)</title>
+      </Head>
       <Banner />
       <About />
     </div>
