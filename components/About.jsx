c4b9665ccef625ea4a116e72cfc224c483c8a878
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-05-13 16:05:00 +0530

    change font size in about section

--- components/About.jsx
+++ components/About.jsx
@@ -4,8 +4,8 @@ const About = () => {
   return (
     <div className='py-8 flex md:flex-row flex-col-reverse bg-gray-200 items-center'>
       <div className='text-green-900 md:w-1/2 px-5'>
-        <div className='text-5xl pb-4 font-medium uppercase'>About Me</div>
-        <div className='lg:text-3xl text-2xl md:leading-snug text-left text-gray-800'>
+        <h1 className='text-4xl pb-4 font-medium uppercase'>About Me</h1>
+        <div className='lg:text-2xl text-xl text-left text-gray-800'>
           I am a college student majoring in computer science and am a tech
           enthusiast. I mostly have experience in full-stack web development,
           app development using Flutter, and artificial intelligence using
@@ -14,7 +14,7 @@ const About = () => {
           contributor and am a part of my college's open source society.
         </div>
       </div>
-      <div className='md:w-1/2 sm:w-4/5 xs:w-full pb-10 px-4 md:pb-0'>
+      <div className='md:w-1/2 sm:w-4/5 xs:w-full pb-10 px-6 md:pb-0'>
         <img src='/dev.svg' alt='dev' className='object-cover h-full w-full' />
       </div>
     </div>
