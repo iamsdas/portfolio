9acafaa2afcc5534eff5915a670c1b212028e826
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-11-02 20:03:58 +0530

    add redirects page

---
+++ pages/redirects/[name].jsx
@@ -0,0 +1,51 @@
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import { useEffect } from 'react';
+
+const Redirects = ({ obj }) => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace(obj.url);
+    }, 1000);
+  }, [obj, router]);
+
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <Head>
+        <meta property='og:image' content={obj?.img} />
+        <meta property='og:url' content={obj?.url} />
+        <meta property='og:title' content={obj?.title} />
+        <meta property='og:description' content={obj?.desc} />
+      </Head>
+      redirecting...
+    </div>
+  );
+};
+
+const redirects = {
+  'skill-verification-system': {
+    url: 'https://learn.figment.io/tutorials/create-a-blockchain-skill-verification-system',
+    img: 'https://figment.io/wp-content/uploads/2019/08/figment-networks-logo.jpg',
+    title: 'Blockchain Based Skill Verification System Tutorial',
+    desc: 'This is a tuorial on "Blockchain based skill verification system" co-written by me and one of my friends',
+  },
+};
+
+export async function getStaticProps(context) {
+  const obj = redirects[context.params.name];
+  return {
+    props: { obj },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: Object.keys(redirects).map((item) => ({
+      params: { name: item },
+    })),
+    fallback: false,
+  };
+}
+
+export default Redirects;
