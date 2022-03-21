b75da48c010c0b6c2ec47affb7a88cb1a1f51d3b
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-03-21 16:40:39 +0530

    fix font loading

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -26,30 +26,10 @@ const Layout = ({ children }) => {
           name='google-site-verification'
           content='_PJHG6L8Nv-7kUrFbYg_ThgZ26chiNe3g7NLFuNVHvw'
         />
-        {/* Fonts */}
-        <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='anonymous'
-        />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Sacramento&display=swap'
-          rel='stylesheet'
-        />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Fira+Code&display=swap'
-          rel='stylesheet'
-        />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Poppins&display=swap'
-          rel='stylesheet'
-        />
       </Head>
       <Navbar />
-      <div className='overflow-y-auto'>
-        {children}
-        <Footer />
-      </div>
+      {children}
+      <Footer />
     </div>
   );
 };
