7fdfc2ff45f217e14cb3a3eed84d5c38a217f49b
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-05-06 00:39:46 +0530

    improve navbar ui

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
@@ -7,13 +8,13 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
   const [navMenu, setNavMenu] = useState(false);
   const stateChange = () => {
-    setTimeout(() => {
-      setNavMenu(!navMenu);
-    }, 200);
+    setNavMenu(!navMenu);
   };
+  const router = useRouter();
+
   return (
-    <header className='fixed inset-x-0 bg-neutral-900 text-neutral-200 z-50'>
-      <div className='flex justify-between px-5 py-3 sm:py-1 items-center'>
+    <header className='fixed inset-x-0 text-neutral-200 z-50'>
+      <div className='flex justify-between px-5 py-4 sm:py-1 items-center bg-neutral-900'>
         <div>
           <span className='text-3xl font-extrabold sm:text-4xl text-amber-300'>
             &lt;
@@ -38,14 +39,24 @@ const Navbar = () => {
         </button>
         <nav className='md:flex gap-3 hidden text-md font-semibold font-para uppercase'>
           <Link href='/'>
-            <a className='hover:text-amber-300'>Home</a>
+            <a
+              className={`underline-offset-2 hover:underline ${
+                router.asPath === '/' ? ' text-amber-300' : ''
+              }`}>
+              Home
+            </a>
           </Link>
           <Link href='/projects'>
-            <a className='hover:text-amber-300'>Projects</a>
+            <a
+              className={`underline-offset-2 hover:underline ${
+                router.asPath === '/projects' ? ' text-amber-300' : ''
+              }`}>
+              Projects
+            </a>
           </Link>
           <a
             href='https://drive.google.com/file/d/1tlo-Fqb1_ICKYVPymqkaYafWiw1dKXv8/view'
-            className='hover:text-amber-300'>
+            className='underline-offset-2 hover:underline'>
             Resume
           </a>
           {/* <Link href='#'>
@@ -55,25 +66,33 @@ const Navbar = () => {
       </div>
       <nav
         className={`${
-          navMenu ? 'flex' : 'hidden'
-        } flex-col gap-3 px-5 pb-5 items-stretch md:hidden text-2xl text-center font-para`}>
+          navMenu
+            ? 'flex bg-neutral-800 opacity-90 transition duration-1000 ease-in-out'
+            : 'hidden'
+        } flex-col gap-3 px-5 pb-5 items-stretch md:hidden text-2xl text-center font-para uppercase`}>
         <Link href='/'>
           <a
-            className='hover:text-yellow-600 border-neutral-700 hover:border-yellow-500 border-t-2 pt-2'
+            className={`border-neutral-700 border-t-2 pt-3 ${
+              router.asPath === '/' ? 'text-yellow-600 border-yellow-500' : ''
+            }`}
             onClick={stateChange}>
             Home
           </a>
         </Link>
         <Link href='/projects'>
           <a
-            className='hover:text-yellow-600 border-neutral-700 hover:border-yellow-500 border-t-2 pt-2'
+            className={`border-neutral-700 border-t-2 pt-3 ${
+              router.asPath === '/projects'
+                ? 'text-yellow-600 border-yellow-500'
+                : ''
+            }`}
             onClick={stateChange}>
             Projects
           </a>
         </Link>
         <a
           href='https://docs.google.com/document/d/1YNOaRup9abZyWiZCYfQTNSKEiIUdgCCG0hTci2BlO1M/export?format=pdf'
-          className='hover:text-amber-300 border-neutral-700 hover:border-yellow-500 border-t-2 pt-2'>
+          className='border-neutral-700 border-t-2 pt-3'>
           Resume
         </a>
         {/* <Link href='#'>
